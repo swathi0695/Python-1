21e58445628442e6d59df48cb5a1d2226b63f79f
ArunSolomon <aruncsolomon@gmail.com>
2019-09-29 22:54:42 +0530

    Flask Blog And Data Science Program

---
+++ Flask_Blog/Blog/flaskblog/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = '8e389bd637a709074a8a5392c0bbba3b'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'email_id'
+    MAIL_PASSWORD = 'password'
