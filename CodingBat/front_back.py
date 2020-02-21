0c6a985a5c29913ef00cb72a2e120510bde83e7e
swathi0695 <37923510+swathi0695@users.noreply.github.com>
2020-02-21 17:33:20 +0530

    Delete front_back.py

--- CodingBat/front_back.py
+++
@@ -1,9 +0,0 @@
-def front_back(str):
-  if len(str) <= 1:
-    return str
-  
-  mid = str[1:len(str)-1]  # can be written as str[1:-1]
-  
-  # last + mid + first
-  # str[len(str)-1] can shorten and write like str[-1]
-  return str[len(str)-1] + mid + str[0]
